031c5150a58c079c28d58a3a4350c7ddeb4e94b3
jyothivmatam <jyothiv@moback.com>
2022-11-06 18:05:08 +0530

    crud mvc task

---
+++ MVCCrud1/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCrud1.Controllers
+{
+    public class HomeController : Controller
+    {
+        MVCCRUDDBContext _context = new MVCCRUDDBContext();
+        public ActionResult Index()
+        {
+            var listofData = _context.StudentInformations.ToList();
+            return View(listofData);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(StudentInformation model)
+        {
+            _context.StudentInformations.Add(model);
+            _context.SaveChanges();
+            ViewBag.Message = "Data Insert Succesfully";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Edit(int id)
+        {
+            var data = _context.StudentInformations.Where(x => x.StudentId == id).FirstOrDefault();
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Edit(StudentInformation Model)
+        {
+            var data = _context.StudentInformations.Where(x => x.StudentId == Model.StudentId).FirstOrDefault();
+            if (data != null)
+            {
+                data.StudentCity = Model.StudentCity;
+                data.StudentName = Model.StudentName;
+                data.StudentFees = Model.StudentFees;
+                _context.SaveChanges();
+
+            }
+            return RedirectToAction("index");
+        }
+        public ActionResult Detail(int id)
+        {
+            var data = _context.StudentInformations.Where(x => x.StudentId == id).FirstOrDefault();
+            return View(data);
+        }
+        public ActionResult Delete(int id)
+        {
+            var data = _context.StudentInformations.Where(x => x.StudentId == id).FirstOrDefault();
+            _context.StudentInformations.Remove(data);
+            _context.SaveChanges();
+            ViewBag.Message = "Record delete Successfully";
+            return RedirectToAction("index");
+        }
+
+
+    }
+}
---
+++ MVCCrud1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCCrud1.MvcApplication" Language="C#" %>
